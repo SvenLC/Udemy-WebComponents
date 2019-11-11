ab27f34892bd07a53a94709728f2dbe73d59d24b
SvenLC <sven.lecann@live.fr>
2019-11-11 12:18:21 +0100

    section 5 -65

--- modal.js
+++ modal.js
@@ -72,15 +72,19 @@ class Modal extends HTMLElement {
                     <slot></slot>
                 </section>
                 <section id="action">
-                    <button>Cancel</button>
-                    <button>Okay</button>
+                    <button id="cancel-btn">Cancel</button>
+                    <button id="confirm-btn">Okay</button>
                 </section>
             </div>
         `;
         const slots = this.shadowRoot.querySelectorAll('slot');
         slots[1].addEventListener('slotchange', event => {
             console.dir(slots[1].assignedNodes());
-        })
+        });
+        const cancelButton = this.shadowRoot.querySelector('#cancel-btn')
+        const confirmButton = this.shadowRoot.querySelector('#confirm-btn')
+        cancelButton.addEventListener('click', this._cancel.bind(this));
+        confirmButton.addEventListener('click', this._confirm.bind(this));
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -104,6 +108,23 @@ class Modal extends HTMLElement {
     open() {
         this.setAttribute('opened', '');
         this.isOpen = true;
+    }
+
+    hide() {
+        if(this.hasAttribute('opened')) {
+            this.removeAttribute('opened');
+        }
+        this.isOpen = false;
+        
+    }
+
+    _cancel() {
+       this.hide();
+
+    }
+
+    _confirm() {
+        this.hide()
     }
 }
 
