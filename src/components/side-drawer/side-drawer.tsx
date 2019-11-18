25a794fb887f289d94798c7a62a1fd1426938968
SvenLC <sven.lecann@live.fr>
2019-11-18 22:39:58 +0100

    Section 6 - 95

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { h, Component, Prop } from '@stencil/core';
+import { h, Component, Prop, State } from '@stencil/core';
 
 
 @Component({
@@ -7,6 +7,7 @@ import { h, Component, Prop } from '@stencil/core';
     shadow: true
 })
 export class SideDrawer {
+    @State() showContactInfo = false;
     @Prop({ reflect: true }) title: string;
     @Prop({ reflect: true }) open: boolean;
 
@@ -15,22 +16,25 @@ export class SideDrawer {
     }
 
     onContentChange(content: string) {
-        console.log(content)
+        this.showContactInfo = content === 'contact';
     }
 
     render() {
         let mainContent = <slot />;
-        mainContent = (
-            <div id="contact-information">
-                <h2>Contact Information</h2>
-                <p>You can reach us via phone or email.</p>
-                <ul>
-                    <li>Phone: 49802384032</li>
-                    <li>E-Mail: <a href="mailto:something@something.com">something@something.com</a></li>
-                </ul>
-            </div>
+        if (this.showContactInfo) {
+            mainContent = (
+                <div id="contact-information">
+                    <h2>Contact Information</h2>
+                    <p>You can reach us via phone or email.</p>
+                    <ul>
+                        <li>Phone: 49802384032</li>
+                        <li>E-Mail: <a href="mailto:something@something.com">something@something.com</a></li>
+                    </ul>
+                </div>
 
-        )
+            )
+        }
+
         return (
             <aside>
                 <header>
@@ -38,8 +42,18 @@ export class SideDrawer {
                     <button onClick={this.onCloseDrawer.bind(this)}>X</button>
                 </header>
                 <section id="tabs">
-                    <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
-                    <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+                    <button
+                        class={!this.showContactInfo ? 'active' : ''}
+                        onClick={this.onContentChange.bind(this, 'nav')}
+                    >
+                        Navigation
+                    </button>
+                    <button
+                        class={this.showContactInfo ? 'active' : ''}
+                        onClick={this.onContentChange.bind(this, 'contact')}
+                    >
+                        Contact
+                    </button>
                 </section>
                 <main>
                     {mainContent}
