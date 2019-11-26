dffc0dcfa15a769e077929a3b366cbf084c3b430
SvenLC <sven.lecann@live.fr>
2019-11-26 23:00:23 +0100

    Section 8 - 109

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,47 +1,64 @@
-import { h, Component, State, Element } from '@stencil/core';
+import { h, Component, State, Element } from "@stencil/core";
 
-import { AV_API_KEY } from '../../global/global';
-
+import { AV_API_KEY } from "../../global/global";
 
 @Component({
-    tag: 'uc-stock-price',
-    styleUrl: './stock-price.css',
-    shadow: true
+  tag: "uc-stock-price",
+  styleUrl: "./stock-price.css",
+  shadow: true
 })
 export class StockPrice {
-    stockInput: HTMLInputElement;
+  stockInput: HTMLInputElement;
 
-    @Element() el: HTMLElement;
+  @Element() el: HTMLElement;
 
-    @State() fetchedPrice: number;
+  @State() fetchedPrice: number;
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
 
-    onFetchStockPrice(event: Event) {
-        event.preventDefault();
-        //const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
-        const stockSymbol = this.stockInput.value;
-        fetch(
-            `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
-        )
-            .then(res => {
-                return res.json();
-            })
-            .then(parsedRes => {
-                this.fetchedPrice = +parsedRes['Global Quote']['05. price']
-            })
-            .catch(err => {
-                console.log(err);
-            });
+  onUserInput(event: Event) {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+    if (this.stockUserInput.trim() !== "") {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
     }
+  }
 
-    render() {
-        return [
-            <form onSubmit={this.onFetchStockPrice.bind(this)}>
-                <input id="stock-symbol" ref={el => this.stockInput = el} />
-                <button type="submit">Fetch</button>
-            </form>,
-            <div>
-                <p>Price: ${this.fetchedPrice} </p>
-            </div>
-        ];
-    }
+  onFetchStockPrice(event: Event) {
+    event.preventDefault();
+    //const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stockInput.value;
+    fetch(
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(parsedRes => {
+        this.fetchedPrice = +parsedRes["Global Quote"]["05. price"];
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input
+          id="stock-symbol"
+          ref={el => (this.stockInput = el)}
+          value={this.stockUserInput}
+          onInput={this.onUserInput.bind(this)}
+        />
+        <button type="submit" disabled={this.stockInputValid}>
+          Fetch
+        </button>
+      </form>,
+      <div>
+        <p>Price: ${this.fetchedPrice} </p>
+      </div>
+    ];
+  }
 }
