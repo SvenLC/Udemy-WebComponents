474f1ef395fb79c53acb047aa6c1e339e1b37457
SvenLC <sven.lecann@live.fr>
2019-12-07 00:06:14 +0100

    Section 8 - 135

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    showStockFinder: false
+  }
   render() {
+    let stockFinder = null;
+    if(this.state.showStockFinder) {
+      stockFinder = <uc-stock-finder/>;
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <uc-stock-price></uc-stock-price>
+        {stockFinder}
+        <button onClick={() => this.setState({showStockFinder: true})}>Show finder</button>
       </div>
     );
   }
